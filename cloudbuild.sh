ed3d146cf3ce71353a42f27dd43f3252dba2d51c
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 14:01:41 -0400

    fix and update cloud build issues

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -10,7 +10,7 @@ do
     ROS_VERSION="$VERSION"
     gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
-    echo Dispached "$MODEL_VERSION" on ROS "$ROS_VERSION"
+    echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
 done
 
